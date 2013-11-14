db4c90a1092ad21100ea1e0820078a909b100117
hermetian <bob.cordwell@tutonic.com>
2013-11-13 21:03:02 -0600

    changing migrations now

--- db/migrate/20131110040005_create_users.rb
+++ db/migrate/20131110040005_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	:user_id, :total_space, :shared_used, :normal_used
       t.integer :total_space
       t.integer :shared_used
       t.integer :normal_used
